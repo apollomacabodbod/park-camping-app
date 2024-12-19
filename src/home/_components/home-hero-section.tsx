76bef893d1224461c1db1cf9e24762f3ada423f3
apollo <apollomacabodbod8@gmail.com>
2024-12-19 09:23:29 +0800

    added ranger camp text

--- src/home/_components/home-hero-section.tsx
+++ src/home/_components/home-hero-section.tsx
@@ -14,11 +14,11 @@ export default function HomeHeroSection(){
 
       <div className="flex items-center">
 
-        <img src="/home-ranger-camp.svg" alt="" />
+        <img src="/home-ranger-camp.svg" alt="" width={27} />
 
-        <p></p>
+        <p className="text-[#F4EEA9]">ranger camp</p>
 
-        <img src="" alt="" />
+        <img src="/home-ranger-camp.svg" alt="" width={27} />
 
 
 
