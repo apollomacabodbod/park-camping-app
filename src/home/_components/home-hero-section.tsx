ba28ae56ac1662304e41ffed03db984172a76875
apollo <apollomacabodbod8@gmail.com>
2024-12-19 09:20:42 +0800

    added home-ranger-camp.svg

---
+++ src/home/_components/home-hero-section.tsx
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+export default function HomeHeroSection(){
+
+  return (<>
+
+
+    <div className="flex flex-col z-10">
+
+
+      <div className="flex items-center">
+
+        <img src="/home-ranger-camp.svg" alt="" />
+
+        <p></p>
+
+        <img src="" alt="" />
+
+
+
+      </div>
+
+
+    </div>
+    
+    
+    
+  </>)
+}
--- src/home/page.tsx
+++ src/home/page.tsx
@@ -1,3 +1,5 @@
+import HomeHeroSection from "./_components/home-hero-section";
+
 export default function Home() {
   
 
@@ -6,11 +8,11 @@ export default function Home() {
     <div className="relative flex flex-col bg-[#BDBDBD] ">
 
 
-
       {/* Overlay */}
       <div className="absolute inset-0 bg-[rgba(16,34,29,0.60)] opacity-80 "></div>
 
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem ut delectus sapiente eos laudantium dolorum vero nisi quod, enim repellendus officiis culpa in tempore beatae velit dolore itaque sed, earum perspiciatis facere. Deleniti officiis veritatis ducimus molestias repudiandae eius, nihil pariatur architecto deserunt vitae, veniam ab sed perferendis maiores dolor placeat nulla cumque voluptates fugiat culpa ut tenetur quae sint? Porro aliquid maxime aperiam amet? Et nobis doloremque voluptatibus commodi dolorum consequatur, iusto nihil quis pariatur debitis sapiente at, sit dicta vitae, officia ad molestiae praesentium. Ipsum, labore quia repellat impedit eligendi sequi illo! Laudantium maxime cum ut, deleniti obcaecati voluptas quisquam perspiciatis saepe mollitia inventore accusantium quasi odio, quibusdam ex dolorum vitae quidem incidunt nesciunt ipsum fugiat unde repudiandae blanditiis iure? Neque facere at delectus quia recusandae quam quos atque iure ducimus, assumenda voluptatibus ipsum aspernatur pariatur eum velit. Perferendis quia iste, officiis ex sequi aliquid, laudantium ipsa saepe deserunt numquam officia libero iure at quasi sint! Asperiores voluptas quidem sapiente, debitis odit recusandae similique mollitia laboriosam provident, molestiae velit qui omnis alias quam beatae excepturi earum repellat, ratione voluptates tempora nostrum obcaecati. Minus possimus optio voluptatibus suscipit voluptatum! Sapiente necessitatibus laboriosam, cumque exercitationem alias eligendi quae et.</p>
+      <HomeHeroSection/>
+
 
 
       <img src="/home-shape.svg" alt="" className="z-10"/>
