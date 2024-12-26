630f176ead6642f3bd717edcd1874ad89c03f96e
apollo <apollomacabodbod8@gmail.com>
2024-12-26 13:41:10 +0800

    optimize images

--- src/home/_components/home-places-cards.tsx
+++ src/home/_components/home-places-cards.tsx
@@ -13,7 +13,7 @@ export default function HomePlacesCards(){
 
         <div className="flex items-center">
 
-          <img src="/home-places-card.svg" alt=""  className="w-full object-cover"/>
+          <img src="/camping-tent.jpg" alt=""  className="w-full  h-[20.625em] rounded-[0.25em]"/>
 
         </div>
 
@@ -42,68 +42,7 @@ export default function HomePlacesCards(){
 
         <div className="flex items-center">
 
-          <img src="/home-places-card.svg" alt=""  className="w-full object-cover"/>
-
-        </div>
-
-
-        <div className="flex items-center justify-between mt-[1em]">
-
-          <p className="text-[#333] font-roboto text-[1.125rem] not-italic font-semibold ">Lake</p>
-
-
-          <img src="/home-places-rating.svg" alt="" />
-
-
-
-        </div>
-
-
-        <p className="text-[#333] font-inter not-italic font-normal text-[0.9375rem] mt-[1em]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-
-
-
-      </div>
-
-
-
-
-
-      <div className="flex flex-col border border-[#50B498] green">
-
-        <div className="flex items-center">
-
-          <img src="/home-places-card.svg" alt=""  className="w-full object-cover"/>
-
-        </div>
-
-
-        <div className="flex items-center justify-between mt-[1em]">
-
-          <p className="text-[#333] font-roboto text-[1.125rem] not-italic font-semibold ">Lake</p>
-
-
-          <img src="/home-places-rating.svg" alt="" />
-
-
-
-        </div>
-
-
-        <p className="text-[#333] font-inter not-italic font-normal text-[0.9375rem] mt-[1em]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-
-
-
-      </div>
-
-
-
-
-      <div className="flex flex-col border border-[#50B498] green">
-
-        <div className="flex items-center">
-
-          <img src="/home-places-card.svg" alt=""  className="w-full object-cover"/>
+          <img src="log-cabin-vintage.jpg" alt=""   className="w-full object-cover h-[20.625em] rounded-[0.25em]"/>
 
         </div>
 
@@ -134,7 +73,68 @@ export default function HomePlacesCards(){
 
         <div className="flex items-center">
 
-          <img src="/home-places-card.svg" alt=""  className="w-full object-cover"/>
+          <img src="/dome-tents.jpg" alt=""   className="w-full object-cover  h-[20.625em] rounded-[0.25em]"/>
+
+        </div>
+
+
+        <div className="flex items-center justify-between mt-[1em]">
+
+          <p className="text-[#333] font-roboto text-[1.125rem] not-italic font-semibold ">Lake</p>
+
+
+          <img src="/home-places-rating.svg" alt="" />
+
+
+
+        </div>
+
+
+        <p className="text-[#333] font-inter not-italic font-normal text-[0.9375rem] mt-[1em]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+
+
+
+      </div>
+
+
+
+
+      <div className="flex flex-col border border-[#50B498] green">
+
+        <div className="flex items-center">
+
+          <img src="/love-couple-relaxing.jpg" alt=""   className="w-full object-cover h-[20.625em] rounded-[0.25em]"/>
+
+        </div>
+
+
+        <div className="flex items-center justify-between mt-[1em]">
+
+          <p className="text-[#333] font-roboto text-[1.125rem] not-italic font-semibold ">Lake</p>
+
+
+          <img src="/home-places-rating.svg" alt="" />
+
+
+
+        </div>
+
+
+        <p className="text-[#333] font-inter not-italic font-normal text-[0.9375rem] mt-[1em]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+
+
+
+      </div>
+
+
+
+
+
+      <div className="flex flex-col border border-[#50B498] green">
+
+        <div className="flex items-center">
+
+          <img src="/smiling-kids.jpg" alt=""   className="w-full object-cover h-[20.625em] rounded-[0.25em]"/>
 
         </div>
 
@@ -163,7 +163,7 @@ export default function HomePlacesCards(){
 
         <div className="flex items-center">
 
-          <img src="/home-places-card.svg" alt=""  className="w-full object-cover"/>
+          <img src="/a-father.jpg" alt="" className="w-full object-cover h-[20.625em] rounded-[0.25em]"/>
 
         </div>
 
