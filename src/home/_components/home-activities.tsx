46c6ecba34b7b194fcc295c8a2a32f61ec03f481
apollo <apollomacabodbod8@gmail.com>
2024-12-30 06:25:56 +0800

    added activities component

---
+++ src/home/_components/home-activities.tsx
@@ -0,0 +1,35 @@
+
+
+
+
+export default function HomeActivities(){
+
+
+  return(<>
+
+    <div className="flex flex-col">
+
+
+      <div className="flex items-center justify-center gap-[0.625em]">
+
+
+        <img src="/home-places.svg" alt="" />
+
+        <p className="text-[#CA6702] leading-normal font-open-sans tracking-[0.125em] text-[0.9375rem] not-italic font-bold uppercase">Activities</p>
+
+        <img src="/home-places.svg" alt="" />
+
+
+      </div>
+
+
+      <p className="text-[#333] font-roboto text-[2.25rem] not-italic font-semibold text-center ">See What Some of Our Guests Are Up To</p>
+
+
+      <p className="text-center font-roboto text-[0.9375rem] not-italic font-normal mt-[2em]">Nullam semper etiam congue lacinia nuncesit quam vel vestibulum<br></br>
+faucibus dolor non semper leo quis pretium quam lacus.</p>
+
+    </div>
+    
+  </>)
+}
