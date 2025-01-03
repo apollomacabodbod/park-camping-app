d08fd4463956252ef0d32ea6547a0e9ece916769
apollo <apollomacabodbod8@gmail.com>
2025-01-03 09:10:56 +0800

    adjust margin buttom

--- src/home/_components/home-activities-video.tsx
+++ src/home/_components/home-activities-video.tsx
@@ -8,7 +8,7 @@ export default function HomeActivtiesVideo(){
   return(<>
 
 
-    <div className="flex flex-col mt-[4em]">
+    <div className="flex flex-col mt-[4em] mb-[6.25em]">
 
 
       <div className="flex items-center justify-center border border-[#50B498] green">
