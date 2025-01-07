b563bfa934264d63ab395dfdbc2b402dc517e8c9
apollo <apollomacabodbod8@gmail.com>
2025-01-07 14:34:58 +0800

    added smooth animation

--- src/blog/_components/blog-read-more.tsx
+++ src/blog/_components/blog-read-more.tsx
@@ -14,7 +14,24 @@ export default function BlogReadMore(){
 
         <div className="flex items-center">
 
-          <img src="/female-enjoying.jpg" alt="" className="w-full h-[27.625em] rounded-[0.25em] object-cover"/>
+          <motion.img src="/female-enjoying.jpg" alt="" className="w-full h-[27.625em] rounded-[0.25em] object-cover transition-all duration-1000 ease-in-out"
+          
+          
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 10 },
+              visible: { 
+                opacity: 1, 
+                y: 0,
+                transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+              },
+            }}
+      
+          
+          
+          />
 
 
 
