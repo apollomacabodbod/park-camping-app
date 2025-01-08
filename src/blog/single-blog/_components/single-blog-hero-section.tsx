48764e01848588f8175144fed46ca83de2a392d4
apollo <apollomacabodbod8@gmail.com>
2025-01-08 09:32:45 +0800

    added useParams hook

--- src/blog/single-blog/_components/single-blog-hero-section.tsx
+++ src/blog/single-blog/_components/single-blog-hero-section.tsx
@@ -1,140 +1,66 @@
 
-import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function SingleBlogHeroSection(){
 
 
-  return(<>
 
-
-    <div className="z-10 flex flex-col max-w-[1200px] lg:mx-auto px-[1.25em] my-[9em] ">
-
-      <div className="flex items-baseline justify-center gap-[0.625em] ">
-
-        <motion.img src="/home-ranger-camp.svg" alt="" width={27}  className="transition-all duration-1000 ease-in-out "
-
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, y: 10 },
-            visible: { 
-              opacity: 1, 
-              y: 0,
-              transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
-            },
-          }}
+       type data = {
+          id : number;
+          date: string;
+          title: string;
+          description: string;
+          image: string;
+       }
   
-
-
-        />
-
-        <motion.p className="text-[#F4EEA9]  font-roboto text-[0.9375rem] not-italic font-bold uppercase transition-all duration-1000 ease-in-out"
-
-
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, y: 10 },
-            visible: { 
-              opacity: 1, 
-              y: 0,
-              transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
-            },
-          }}
   
-
-        >Blog</motion.p>
-
-        <motion.img src="/home-ranger-camp.svg" alt="" width={27} className="transition-all duration-1000 ease-in-out"
-
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, y: 10 },
-            visible: { 
-              opacity: 1, 
-              y: 0,
-              transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
-            },
-          }}
-  
-
-        />
-
-
-
-      </div>
-
-
-      <motion.p className="text-white text-center font-roboto text-[4.5rem] not-italic font-bold leading-[1em] transition-all duration-1000 ease-in-out"
+       const [cards] = useState<data[]>(
+         [
+           { id: 1, date: "December 30, 2021  No Comments", title: "Nullam semper etiam congue lacinia lorem", description: "Nullam semper etiam congue lacinia nuncesit quam vel vestibulum faucibus dolor semper.", image: "/camping-2023.jpg" },
+           { id: 2, date: "December 30, 2021  No Comments", title: "Nullam semper etiam congue lacinia loren", description: "Nullam semper etiam congue lacinia nuncesit quam vel vestibulum faucibus dolor semper.", image: "/camping-with-red.jpg" },
+           { id: 3, date: "December 30, 2021  No Comments", title: "Nullam semper etiam congue lacinia lorex", description: "Nullam semper etiam congue lacinia nuncesit quam vel vestibulum faucibus dolor semper.", image: "/camping-mornings.jpg" },
+           { id: 4, date: "December 30, 2021  No Comments", title: "Nullam semper etiam congue lacinia lorev", description: "Nullam semper etiam congue lacinia nuncesit quam vel vestibulum faucibus dolor semper.", image: "/family-camping.jpg" },
+           { id: 5, date: "December 30, 2021  No Comments", title: "Nullam semper etiam congue lacinia lored", description: "Nullam semper etiam congue lacinia nuncesit quam vel vestibulum faucibus dolor semper.", image: "/friends-cooking.jpg" },
+           { id: 6, date: "December 30, 2021  No Comments", title: "Nullam semper etiam congue lacinia loreq", description: "Nullam semper etiam congue lacinia nuncesit quam vel vestibulum faucibus dolor semper.", image: "/friends-having.jpg" },
+           { id: 7, date: "December 30, 2021  No Comments", title: "Nullam semper etiam congue lacinia loreh", description: "Nullam semper etiam congue lacinia nuncesit quam vel vestibulum faucibus dolor semper.", image: "/tourist-camp.jpg" },
+           { id: 8, date: "December 30, 2021  No Comments", title: "Nullam semper etiam congue lacinia lorej", description: "Nullam semper etiam congue lacinia nuncesit quam vel vestibulum faucibus dolor semper.", image: "/tourist-couple.jpg" },
+           { id: 9, date: "December 30, 2021  No Comments", title: "Nullam semper etiam congue lacinia lorei", description: "Nullam semper etiam congue lacinia nuncesit quam vel vestibulum faucibus dolor semper.", image: "/tourists-go.jpg" },
       
       
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.5 }}
-        variants={{
-          hidden: { opacity: 0, y: 10 },
-          visible: { 
-            opacity: 1, 
-            y: 0,
-            transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
-          },
-        }}
+         ]
+       )
+
+
+       const { id } = useParams()
+       const blogId = Number(id); // Convert to a number for comparison
+
+       const blog = cards.find((blog) => blog.id === blogId ) 
+
 
       
-      
-      >Blog</motion.p>
+
+       return(<>
 
 
-
-      <motion.div className="flex items-baseline justify-center gap-[1em] mt-[2em] transition-all duration-1000 ease-in-out"
-      
-      
-      
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.5 }}
-        variants={{
-          hidden: { opacity: 0, y: 10 },
-          visible: { 
-            opacity: 1, 
-            y: 0,
-            transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
-          },
-        }}
-
-      
-      
-      >
+         <div className="z-10 flex flex-col max-w-[1200px] lg:mx-auto px-[1.25em]  my-[9em] ">
 
 
-        <img src="/services-home.svg" alt="" />
+           <p className="text-white font-montserrat text-[4.5rem] text-center not-italic font-bold max-w-[15em]">{blog?.title}</p>
 
 
-        <Link to={"/"} className="text-white font-roboto text-[0.9375rem] not-italic font-bold uppercase"> Home</Link>
-
-
-
-        <img src="/services-arrow.svg" alt="" />
-
-
-        <Link to={"/services"} className="text-white font-roboto text-[0.9375rem] not-italic font-bold uppercase">Blog</Link>
+           <p className="text-[#F4EEA9] font-inter text-[0.9375rem] text-center not-italic font-normal ">December 30, 2021  No Comments</p>
 
 
 
 
-      </motion.div>
+
+         </div>
 
 
-
-    </div>
     
     
     
-  </>)
+       </>)
 }
