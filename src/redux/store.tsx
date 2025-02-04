5464ded114b43f2c4cdc539724335c721b767d45
apollo <apollomacabodbod8@gmail.com>
2025-02-04 18:11:21 +0800

    added store

---
+++ src/redux/store.tsx
@@ -0,0 +1,18 @@
+import { useSelector, TypedUseSelectorHook } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+// import globalReducer from "@/redux/features/globalSlice";
+// import coinMarketReducer from "@/redux/features/coinMarketSlice";
+
+export const store = configureStore({
+//   reducer: {
+//     globalData: globalReducer,
+//     coinMarketData: coinMarketReducer,
+//   },
+
+  reducer: {}, // Add your reducers here
+
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
