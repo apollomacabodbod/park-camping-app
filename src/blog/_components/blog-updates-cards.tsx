05e59b7242e4005bbfbf439ebba0e01cfe3ad22c
apollo <apollomacabodbod8@gmail.com>
2025-02-04 19:00:10 +0800

    remove unused variable

--- src/blog/_components/blog-updates-cards.tsx
+++ src/blog/_components/blog-updates-cards.tsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { motion } from "framer-motion"
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
